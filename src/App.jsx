d8f5bd435c1eb2dd6fb86736c559197390beca34
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-04-18 18:45:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Book Vibe</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
