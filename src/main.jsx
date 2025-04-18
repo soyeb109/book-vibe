b3c0b46de13fe28c08a83ea5a64232b97b2e849c
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-04-18 23:54:51 +0600

    implement navbar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
-
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+import { RouterProvider } from "react-router";
+import { router } from "./Routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
