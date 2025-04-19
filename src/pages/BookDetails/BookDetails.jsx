cff60383e523b3d7224d74dbcb379a88a6cb9127
Md. Soyeb Islam <soyeb10191@gmail.com>
2025-04-19 09:57:00 +0600

    book details page

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const BookDetails = () => {
-  return <div>BookDetails</div>;
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  const data = useLoaderData();
+  const singleBook = data.find((book) => book.bookId === bookId);
+  // console.log(singleBook);
+  const {
+    bookName,
+    image,
+    review,
+    totalPages,
+    author,
+    category,
+    tags,
+    yearOfPublishing,
+    publisher,
+    rating,
+  } = singleBook;
+
+  return (
+    <div className="hero bg-base-200 min-h-screen ">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={image} className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+          <h1 className="text-5xl font-bold py-2">{bookName}</h1>
+          <p className="my-2">By {author}</p>
+          <p className="my-2"> {category}</p>
+          <p>
+            <span className="text-md mr-2 font-bold ">Review:</span>
+            {review}
+          </p>
+          <div className="flex justify-start gap-2 my-2">
+            <h1 className="font-bold mr-2 my-2">Tag</h1>
+            {tags.map((tag) => (
+              <button className="text-green-400">{tag}</button>
+            ))}
+          </div>
+          <div className="flex flex-col gap-4 mb-2">
+            <p className="flex gap-2">
+              <span>Number of Pages:</span>
+              <span>{totalPages}</span>
+            </p>
+            <p className="flex gap-2">
+              <span>Publish Date:</span>
+              <span>{yearOfPublishing}</span>
+            </p>
+            <p className="flex gap-2">
+              <span>Publisher:</span>
+              <span>{publisher}</span>
+            </p>
+            <p className="flex gap-2">
+              <span>Rating:</span>
+              <span>{rating}</span>
+            </p>
+          </div>
+          <div className="flex justify-end">
+            {" "}
+            <button className="btn btn-ghost">Read</button>
+            <button className="btn btn-primary">Wishlist</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookDetails;
